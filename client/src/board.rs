c8e070a264713544de4f1e049e545b7c9e5d2917
Jani Mustonen <janijohannes@kapsi.fi>
2020-09-13 18:22:24 +0300

    slight rendering cleanup & borders

--- client/src/board.rs
+++ client/src/board.rs
@@ -22,6 +22,7 @@ pub struct Board {
     selection_pos: Option<(u32, u32)>,
     width: u32,
     height: u32,
+    edge_size: i32,
 }
 
 #[derive(Properties, Clone, PartialEq)]
@@ -53,6 +54,7 @@ impl Component for Board {
             selection_pos: None,
             width: 0,
             height: 0,
+            edge_size: 10,
         }
     }
 
@@ -155,16 +157,26 @@ impl Component for Board {
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         let game = &self.props.game;
-        let width = self.width as f64;
-        let mouse_to_coord = |p: (f64, f64)| match game.mods.pixel {
-            true => (
-                (p.0 / (width / game.size.0 as f64) + 0.5) as u32,
-                (p.1 / (width / game.size.1 as f64) + 0.5) as u32,
-            ),
-            false => (
-                (p.0 / (width / game.size.0 as f64)) as u32,
-                (p.1 / (width / game.size.1 as f64)) as u32,
-            ),
+        let edge_size = self.edge_size as f64;
+        let width = self.width as f64 - (edge_size);
+        let height = self.height as f64 - (edge_size);
+        let mouse_to_coord = |mut p: (f64, f64)| -> Option<(u32, u32)> {
+            if p.0 < edge_size || p.1 < edge_size || p.0 > width || p.1 > height {
+                return None;
+            }
+
+            p.0 -= edge_size;
+            p.1 -= edge_size;
+            Some(match game.mods.pixel {
+                true => (
+                    (p.0 / (width / game.size.0 as f64) + 0.5) as u32,
+                    (p.1 / (width / game.size.1 as f64) + 0.5) as u32,
+                ),
+                false => (
+                    (p.0 / (width / game.size.0 as f64)) as u32,
+                    (p.1 / (width / game.size.1 as f64)) as u32,
+                ),
+            })
         };
 
         match msg {
@@ -173,7 +185,7 @@ impl Component for Board {
             }
             Msg::MouseMove(p) => {
                 self.mouse_pos = Some(p);
-                self.selection_pos = Some(mouse_to_coord(p));
+                self.selection_pos = mouse_to_coord(p);
                 self.render_gl(0.0).unwrap();
             }
             Msg::Click(p) => {
@@ -182,11 +194,13 @@ impl Component for Board {
                     return false;
                 }
                 self.mouse_pos = Some(p);
-                self.selection_pos = Some(mouse_to_coord(p));
-                networking::send(ClientMessage::GameAction(GameAction::Place(
-                    self.selection_pos.unwrap().0,
-                    self.selection_pos.unwrap().1,
-                )));
+                self.selection_pos = mouse_to_coord(p);
+                if let Some(selection_pos) = self.selection_pos {
+                    networking::send(ClientMessage::GameAction(GameAction::Place(
+                        selection_pos.0,
+                        selection_pos.1,
+                    )));
+                }
             }
             Msg::MouseLeave => {
                 self.mouse_pos = None;
@@ -206,7 +220,8 @@ impl Component for Board {
 
 impl Board {
     fn render_gl(&mut self, _timestamp: f64) -> Result<(), JsValue> {
-        // TODO: remove hardcoding for 19x19
+        // Stone colors ///////////////////////////////////////////////////////
+
         let shadow_stone_colors = ["#555555", "#bbbbbb", "#7b91bd", "#e09db4"];
         let shadow_border_colors = ["#bbbbbb", "#555555", "#555555", "#555555"];
         let stone_colors = ["#000000", "#eeeeee", "#5074bc", "#e0658f"];
@@ -214,20 +229,15 @@ impl Board {
         let border_colors = ["#555555", "#000000", "#000000", "#000000"];
         let dead_mark_color = ["#eeeeee", "#000000", "#000000", "#000000"];
 
+        let edge_size = self.edge_size as f64;
+
+        // Setup //////////////////////////////////////////////////////////////
+
         let context = self
             .canvas2d
             .as_ref()
             .expect("Canvas Context not initialized!");
         let canvas = self.canvas.as_ref().expect("Canvas not initialized!");
-
-        context.clear_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
-
-        context.set_fill_style(&JsValue::from_str("#e0bb6c"));
-        context.fill_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
-
-        context.set_line_width(1.0);
-        context.set_stroke_style(&JsValue::from_str("#000000"));
-        context.set_fill_style(&JsValue::from_str("#000000"));
 
         let game = &self.props.game;
         let board = match &game.history {
@@ -241,25 +251,63 @@ impl Board {
 
         // TODO: actually handle non-square boards
         let board_size = game.size.0 as usize;
-        let size = canvas.width() as f64 / board_size as f64;
+        let size = (canvas.width() as f64 - 2.0 * edge_size) / board_size as f64;
         let turn = game.seats[game.turn as usize].1;
 
-        // Lines
+        let draw_stone =
+            |(x, y): (i32, i32), diameter: f64, fill: bool, stroke: bool| -> Result<(), JsValue> {
+                context.begin_path();
+                context.arc(
+                    edge_size + (x as f64 + 0.5) * size,
+                    edge_size + (y as f64 + 0.5) * size,
+                    diameter / 2.,
+                    0.0,
+                    2.0 * std::f64::consts::PI,
+                )?;
+                if fill {
+                    context.fill();
+                }
+                if stroke {
+                    context.stroke();
+                }
+                Ok(())
+            };
+
+        // Clear canvas ///////////////////////////////////////////////////////
+
+        context.clear_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
+
+        context.set_fill_style(&JsValue::from_str("#e0bb6c"));
+        context.fill_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
+
+        // Board lines ////////////////////////////////////////////////////////
+
+        context.set_line_width(1.0);
+        context.set_stroke_style(&JsValue::from_str("#000000"));
+        context.set_fill_style(&JsValue::from_str("#000000"));
+
         for y in 0..game.size.1 {
             context.begin_path();
-            context.move_to(size * 0.5, (y as f64 + 0.5) * size);
-            context.line_to(size * (board_size as f64 - 0.5), (y as f64 + 0.5) * size);
+            context.move_to(edge_size + size * 0.5, edge_size + (y as f64 + 0.5) * size);
+            context.line_to(
+                edge_size + size * (board_size as f64 - 0.5),
+                edge_size + (y as f64 + 0.5) * size,
+            );
             context.stroke();
         }
 
         for x in 0..game.size.0 {
             context.begin_path();
-            context.move_to((x as f64 + 0.5) * size, size * 0.5);
-            context.line_to((x as f64 + 0.5) * size, size * (board_size as f64 - 0.5));
+            context.move_to(edge_size + (x as f64 + 0.5) * size, edge_size + size * 0.5);
+            context.line_to(
+                edge_size + (x as f64 + 0.5) * size,
+                edge_size + size * (board_size as f64 - 0.5),
+            );
             context.stroke();
         }
 
-        // Starpoints - by popular demand
+        // Starpoints /////////////////////////////////////////////////////////
+
         let points: &[(u8, u8)] = match game.size.0 {
             19 => &[
                 (3, 3),
@@ -277,16 +325,10 @@ impl Board {
             _ => &[],
         };
         for &(x, y) in points {
-            context.begin_path();
-            context.arc(
-                (x as f64 + 0.5) * size,
-                (y as f64 + 0.5) * size,
-                size / 8.,
-                0.0,
-                2.0 * std::f64::consts::PI,
-            )?;
-            context.fill();
+            draw_stone((x as _, y as _), size / 4., true, false)?;
         }
+
+        // Mouse hover display ////////////////////////////////////////////////
 
         if let Some(selection_pos) = self.selection_pos {
             let mut p = (selection_pos.0 as i32, selection_pos.1 as i32);
@@ -296,6 +338,7 @@ impl Board {
             }
 
             // TODO: This allocation is horrible, figure out how to avoid it
+            // TODO: Also move these to shared
             let points = match game.mods.pixel {
                 true => vec![
                     (p.0, p.1),
@@ -312,18 +355,14 @@ impl Board {
             context.set_stroke_style(&JsValue::from_str(shadow_border_colors[color as usize - 1]));
 
             for p in points {
-                context.begin_path();
-                context.arc(
-                    (p.0 as f64 + 0.5) * size,
-                    (p.1 as f64 + 0.5) * size,
-                    size / 2.,
-                    0.0,
-                    2.0 * std::f64::consts::PI,
-                )?;
-                context.fill();
-                context.stroke();
+                if p.0 < 0 || p.1 < 0 || p.0 >= game.size.0 as i32 || p.1 >= game.size.1 as i32 {
+                    continue;
+                }
+                draw_stone(p, size, true, true)?;
             }
         }
+
+        // Board stones ///////////////////////////////////////////////////////
 
         for (idx, &color) in board.iter().enumerate() {
             let x = idx % board_size;
@@ -339,22 +378,12 @@ impl Board {
             }
 
             context.set_fill_style(&JsValue::from_str(stone_colors[color as usize - 1]));
-
             context.set_stroke_style(&JsValue::from_str(border_colors[color as usize - 1]));
 
-            let size = canvas.width() as f64 / board_size as f64;
-            // create shape of radius 'size' around center point (size, size)
-            context.begin_path();
-            context.arc(
-                (x as f64 + 0.5) * size,
-                (y as f64 + 0.5) * size,
-                size / 2.,
-                0.0,
-                2.0 * std::f64::consts::PI,
-            )?;
-            context.fill();
-            context.stroke();
+            draw_stone((x as _, y as _), size, true, true)?;
         }
+
+        // Hidden stones //////////////////////////////////////////////////////
 
         for (idx, &colors) in board_visibility.iter().flatten().enumerate() {
             let x = idx % board_size;
@@ -368,29 +397,20 @@ impl Board {
 
             for color in &colors {
                 context.set_fill_style(&JsValue::from_str(stone_colors_hidden[color as usize - 1]));
-
                 context.set_stroke_style(&JsValue::from_str(border_colors[color as usize - 1]));
 
-                let size = canvas.width() as f64 / board_size as f64;
-                // create shape of radius 'size' around center point (size, size)
-                context.begin_path();
-                context.arc(
-                    (x as f64 + 0.5) * size,
-                    (y as f64 + 0.5) * size,
-                    size / 2.,
-                    0.0,
-                    2.0 * std::f64::consts::PI,
-                )?;
-                context.fill();
-                context.stroke();
+                draw_stone((x as _, y as _), size, true, true)?;
             }
         }
+
+        // Last stone marker //////////////////////////////////////////////////
 
         let last_stone = match (&game.state, &game.history) {
             (_, Some(h)) => h.last_stone.as_ref(),
             (GameState::Play(state), _) => state.last_stone.as_ref(),
             _ => None,
         };
+
         if let Some(points) = last_stone {
             for &(x, y) in points {
                 let mut color = board[y as usize * game.size.0 as usize + x as usize];
@@ -403,73 +423,75 @@ impl Board {
                 context.set_stroke_style(&JsValue::from_str(dead_mark_color[color as usize - 1]));
                 context.set_line_width(2.0);
 
-                let size = canvas.width() as f64 / board_size as f64;
-                // create shape of radius 'size' around center point (size, size)
-                context.begin_path();
-                context.arc(
-                    (x as f64 + 0.5) * size,
-                    (y as f64 + 0.5) * size,
-                    size / 4.,
-                    0.0,
-                    2.0 * std::f64::consts::PI,
-                )?;
-                context.stroke();
+                draw_stone((x as _, y as _), size / 2., false, true)?;
             }
-        } else if game.history.is_none() {
-            match &game.state {
-                GameState::Scoring(scoring) | GameState::Done(scoring) => {
-                    for group in &scoring.groups {
-                        if group.alive {
-                            continue;
-                        }
+        }
 
-                        for &(x, y) in &group.points {
-                            context.set_line_width(2.0);
-                            context.set_stroke_style(&JsValue::from_str(
-                                dead_mark_color[group.team.0 as usize - 1],
-                            ));
+        // States /////////////////////////////////////////////////////////////
 
-                            context.set_stroke_style(&JsValue::from_str(
-                                dead_mark_color[group.team.0 as usize - 1],
-                            ));
-
-                            context.begin_path();
-                            context.move_to((x as f64 + 0.2) * size, (y as f64 + 0.2) * size);
-                            context.line_to((x as f64 + 0.8) * size, (y as f64 + 0.8) * size);
-                            context.stroke();
-
-                            context.begin_path();
-                            context.move_to((x as f64 + 0.8) * size, (y as f64 + 0.2) * size);
-                            context.line_to((x as f64 + 0.2) * size, (y as f64 + 0.8) * size);
-                            context.stroke();
-                        }
+        match &game.state {
+            GameState::Scoring(scoring) | GameState::Done(scoring) => {
+                for group in &scoring.groups {
+                    if group.alive {
+                        continue;
                     }
 
-                    for (idx, &color) in scoring.points.points.iter().enumerate() {
-                        let x = (idx % board_size) as f64;
-                        let y = (idx / board_size) as f64;
-
-                        if color.is_empty() {
-                            continue;
-                        }
-
-                        context
-                            .set_fill_style(&JsValue::from_str(stone_colors[color.0 as usize - 1]));
-
+                    for &(x, y) in &group.points {
+                        context.set_line_width(2.0);
                         context.set_stroke_style(&JsValue::from_str(
-                            border_colors[color.0 as usize - 1],
+                            dead_mark_color[group.team.0 as usize - 1],
                         ));
 
-                        context.fill_rect(
-                            (x + 1. / 3.) * size,
-                            (y + 1. / 3.) * size,
-                            (1. / 3.) * size,
-                            (1. / 3.) * size,
+                        context.set_stroke_style(&JsValue::from_str(
+                            dead_mark_color[group.team.0 as usize - 1],
+                        ));
+
+                        context.begin_path();
+                        context.move_to(
+                            edge_size + (x as f64 + 0.2) * size,
+                            edge_size + (y as f64 + 0.2) * size,
                         );
+                        context.line_to(
+                            edge_size + (x as f64 + 0.8) * size,
+                            edge_size + (y as f64 + 0.8) * size,
+                        );
+                        context.stroke();
+
+                        context.begin_path();
+                        context.move_to(
+                            edge_size + (x as f64 + 0.8) * size,
+                            edge_size + (y as f64 + 0.2) * size,
+                        );
+                        context.line_to(
+                            edge_size + (x as f64 + 0.2) * size,
+                            edge_size + (y as f64 + 0.8) * size,
+                        );
+                        context.stroke();
                     }
                 }
-                _ => {}
+
+                for (idx, &color) in scoring.points.points.iter().enumerate() {
+                    let x = (idx % board_size) as f64;
+                    let y = (idx / board_size) as f64;
+
+                    if color.is_empty() {
+                        continue;
+                    }
+
+                    context.set_fill_style(&JsValue::from_str(stone_colors[color.0 as usize - 1]));
+
+                    context
+                        .set_stroke_style(&JsValue::from_str(border_colors[color.0 as usize - 1]));
+
+                    context.fill_rect(
+                        edge_size + (x + 1. / 3.) * size,
+                        edge_size + (y + 1. / 3.) * size,
+                        edge_size + (1. / 3.) * size,
+                        edge_size + (1. / 3.) * size,
+                    );
+                }
             }
+            _ => {}
         }
 
         let render_frame = self.link.callback(Msg::Render);
