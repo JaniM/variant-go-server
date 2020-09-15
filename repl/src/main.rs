a69803af41c118f98a740d2bdeb1f22f51b6f406
Jani Mustonen <janijohannes@kapsi.fi>
2020-09-15 23:45:29 +0300

    room pruning tools

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,9 +1,23 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
+
 use futures_util::{future, pin_mut, StreamExt};
-use shared::message::ServerMessage;
-use tokio::io::AsyncWriteExt;
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
-use shared::message::{AdminAction, ClientMessage};
+use shared::message::{AdminAction, ClientMessage, ServerMessage};
+
+#[derive(Default)]
+struct RoomInfo {
+    room_id: u32,
+    member_count: usize,
+    move_count: usize,
+}
+
+#[derive(Default)]
+struct State {
+    rooms: HashMap<u32, RoomInfo>,
+}
 
 fn pack(msg: ClientMessage) -> Vec<u8> {
     serde_cbor::to_vec(&msg).unwrap()
@@ -15,9 +29,11 @@ async fn main() {
 
     let url = url::Url::parse("ws://localhost:8088/ws/").unwrap();
 
+    let state = Arc::new(Mutex::new(State::default()));
+
     let (ws_tx, ws_rx) = futures_channel::mpsc::unbounded();
 
-    tokio::spawn(read_stdin(ws_tx));
+    tokio::spawn(read_stdin(state.clone(), ws_tx));
 
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
     println!("WebSocket handshake has been successfully completed");
@@ -26,16 +42,44 @@ async fn main() {
 
     let stdin_to_ws = ws_rx.map(Ok).forward(write);
     let ws_to_stdout = {
-        read.for_each(|message| async {
+        let state = state.clone();
+        read.for_each(move |message| {
             if let Ok(Message::Binary(data)) = message {
                 let msg = serde_cbor::from_slice::<ServerMessage>(&data).unwrap();
-                let mut stdout = tokio::io::stdout();
-                stdout
-                    .write_all(format!("{:?}\n", msg).as_bytes())
-                    .await
-                    .unwrap();
-                stdout.flush().await.unwrap();
+                #[allow(clippy::single_match)]
+                match msg {
+                    ServerMessage::GameStatus {
+                        room_id,
+                        move_number,
+                        members,
+                        ..
+                    } => {
+                        let mut state = state.lock().unwrap();
+                        let room = state.rooms.entry(room_id).or_insert_with(RoomInfo::default);
+                        room.room_id = room_id;
+                        room.move_count = move_number as usize;
+                        room.member_count = members.len() - 1; // Subtract self
+                        println!(
+                            "Visited room {}: {} players, {} moves",
+                            room_id, room.member_count, room.move_count
+                        );
+                    }
+                    ServerMessage::AnnounceGame { room_id, name } => {
+                        let mut state = state.lock().unwrap();
+                        let room = state.rooms.entry(room_id).or_insert_with(RoomInfo::default);
+                        room.room_id = room_id;
+                        println!("New room {}: {:?}", room_id, name);
+                    }
+                    ServerMessage::CloseGame { room_id } => {
+                        let mut state = state.lock().unwrap();
+                        if state.rooms.remove(&room_id).is_some() {
+                            println!("Closed room {}", room_id);
+                        }
+                    }
+                    _ => {}
+                }
             }
+            async {}
         })
     };
 
@@ -45,7 +89,7 @@ async fn main() {
 
 // Our helper method which will read data from stdin and send it along the
 // sender provided.
-async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
+async fn read_stdin(state: Arc<Mutex<State>>, tx: futures_channel::mpsc::UnboundedSender<Message>) {
     use tokio::io::{self, AsyncBufReadExt, BufReader};
 
     let token = std::env::var("ADMIN_TOKEN").unwrap();
@@ -103,6 +147,29 @@ async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
                     .collect(),
                 _ => vec![],
             },
+            "list" | "li" => vec![ClientMessage::GetGameList],
+            "visit" | "v" => {
+                let state = state.lock().unwrap();
+                state
+                    .rooms
+                    .values()
+                    .map(|x| ClientMessage::JoinGame(x.room_id))
+                    .collect()
+            }
+            "prune" | "p" => match words.next() {
+                Some("below") | Some("b") => {
+                    let limit: usize = words.next().and_then(|x| x.parse().ok()).unwrap_or(0);
+                    let state = state.lock().unwrap();
+                    state
+                        .rooms
+                        .values()
+                        .filter(|x| x.move_count < limit && x.member_count == 0)
+                        .map(|x| ClientMessage::Admin(AdminAction::UnloadRoom(x.room_id)))
+                        .collect()
+                }
+                _ => vec![],
+            },
+            "quit" | "q" => std::process::exit(0),
             _ => vec![],
         };
 
